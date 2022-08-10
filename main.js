3d100465a503938c43088e534133530401c67112
Anthony Roccasalva <anthony.roccasalva@gmail.com>
2022-08-10 18:06:29 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,16 @@
+const add = function (x, y) {
+    return (x + y);
+};
+
+const subtract = function (x, y) {
+    return (x - y);
+};
+
+
+const multiply = function (x, y) {
+    return (x * y);
+};
+
+const divide = function (x, y) {
+    return (x / y);
+}
